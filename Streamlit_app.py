85c0813aa0f9f1f60f6ec7bab4ad50386aa9b250
HarshJadhav100 <hjadhav866@gmail.com>
2025-10-15 12:25:10 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -64,10 +64,10 @@ def main():
     st.markdown(title_css, unsafe_allow_html=True)
 
     # Sidebar
-    st.sidebar.write("<h1 style='color: #F63366; font-size: 36px;'>Group 12</h1>", unsafe_allow_html=True)
+    st.sidebar.write("<h1 style='color: #F63366; font-size: 36px;'>Group 17</h1>", unsafe_allow_html=True)
     
 
-    st.sidebar.write("GitHub profile : (https://github.com/HarshJadhav100/thyroid-disesase-detection_using_streamlit)")
+    #st.sidebar.write("GitHub profile : (https://github.com/HarshJadhav100/thyroid-disesase-detection_using_streamlit)")
 
     st.sidebar.title("About Project :")
     st.sidebar.write("This Streamlit app serves as a Thyroid Diagnosis Predictor. It utilizes machine learning to predict thyroid diagnosis based on various patient attributes such as age, sex, medical history, and laboratory test results. Users can input patient data and receive an immediate diagnosis prediction, helping medical professionals make informed decisions efficiently.")
